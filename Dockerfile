0601e7538bca347d7d974949d258c2a36058f024
Christopher <christopher@stackocean.io>
2023-01-13 13:09:09 +0100

    filled placeholder code with actual prediciton

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM python:3.10-slim-bullseye
+FROM python:3.8
 
-WORKDIR /app
+# 
+WORKDIR /code
+COPY . .
 
-COPY  . .
+# 
+RUN pip install -r requirements.txt
 
-RUN pip install --no-cache-dir --upgrade -r requirements.txt
-
-EXPOSE 8000
-
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+# 
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/main.py
+++ app/main.py
@@ -1,14 +1,54 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 
-class Input(BaseModel):
-    text: str
+import fasttext
+from fasttext import tokenize
+import re
+import os
 
 app = FastAPI()
 
+fasttext_language_model = fasttext.load_model(os.path.join("model", "lid.176.ftz"))
 
-@app.post("/")
-async def detect_language(input: Input):
-    print('Incoming request with text "{}"'.format(input.text))
-    # Return mock data
-    return {"language": "de"}
+white_space_pattern = re.compile(r"\s")
+
+
+def preprocess_text_for_language_detection(text: str):
+    """
+    Cleans the text as per fasttext requirements.
+    The requirements can be found here: https://pypi.org/project/fasttext/
+    
+    :text: str: text to clean
+    :returns: str: cleaned text
+    """
+    # fastText assumes UTF-8 encoded text
+    text = str(text)
+    
+    # fastText is not aware of UTF-8 whitespace
+    # Replace all white space with space
+    text = white_space_pattern.sub(text, " ")
+    
+    # Tokenize text, per fastext function and rejoin
+    tokens = tokenize(text)
+    text = " ".join(tokens)
+    n = len(tokens)
+    
+    # Remove white space char as it affects the model accuracy
+    text = text.replace("</s>", "")
+    
+    return text.lower()
+
+
+@app.get("/language_detect/")
+def identify_languages(text: str, no_of_languages: int =1, threshold: float=0.0):
+
+    if len(text) == 0:
+        raise HTTPException(status_code=406, detail="Text field is empty")
+    clean_text = preprocess_text_for_language_detection(text)
+    if len(clean_text) == 0:
+        raise HTTPException(status_code=406, detail="Cleaned text is empty")
+    ft_output = fasttext_language_model.predict(clean_text, no_of_languages, threshold=threshold)
+    # format output
+    result = [(ft_output[0][i][-2:], ft_output[1][i]) for i in range(len(ft_output[0]))]
+    return result
+
+print(identify_languages("the door is open", 4))

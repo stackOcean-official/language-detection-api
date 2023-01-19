6116a4e8bd8bf572d9572b9bb5c5b3999209c38d
Matthias Nannt <mail@matthiasnannt.com>
2023-01-19 14:11:38 +0100

    update env variable in docker-compose

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import Depends, FastAPI
 from pydantic import BaseModel
 
 from app.api import Input, Output, get_api_key
-from app.language_detection import (handle_detect_language_request)
+from app.language_detection import handle_detect_language_request
 
 app = FastAPI(dependencies=[Depends(get_api_key)])
 
